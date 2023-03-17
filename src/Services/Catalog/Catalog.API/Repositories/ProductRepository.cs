11ac7510f9669b6500315aaadeeb1a47f73b08b4
zakariabijoy <zakaria.bijoy@live.com>
2023-03-18 01:48:33 +0600

    Developing Business Layer - Repository Pattern on Catalog.API Microservice

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ICatalogContext _context;
+
+    public ProductRepository(ICatalogContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts() => await _context.Products.Find(p => true).ToListAsync();
+
+    public async Task<Product> GetProduct(string id) => await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+    public async Task<IEnumerable<Product>> GetProductsByName(string name)
+    {
+        FilterDefinition<Product> filters = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+        return await _context.Products.Find(filters).ToListAsync(); 
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsByCategory(string categoryName)
+    {
+        FilterDefinition<Product> filters = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+        return await _context.Products.Find(filters).ToListAsync();
+    }
+
+
+    public async Task CreateProduct(Product product) => await _context.Products.InsertOneAsync(product);
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var updatedResult =  await _context.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, product);
+
+        return updatedResult.IsAcknowledged && updatedResult.MatchedCount > 0;
+    }
+
+    public async Task<bool> DeleteProduct(string id)
+    {
+        FilterDefinition<Product> filters = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+        DeleteResult deleteResult = await _context.Products.DeleteOneAsync(filters);
+
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
+}
