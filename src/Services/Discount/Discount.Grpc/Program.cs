6e9a794b9ea07bfeafe0988f28cf43ec7b8ca11d
zakariabijoy <zakaria.bijoy@live.com>
2023-03-25 02:55:21 +0600

    Managing PostreSQL Database Operations in Discount Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
@@ -13,5 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MigrateDatabase<Program>();
 
 app.Run();
