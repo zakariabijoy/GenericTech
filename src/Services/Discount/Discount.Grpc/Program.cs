ca1f8d3ba2ef04ede1058dc9fbbd741c248bb3e3
zakariabijoy <zakaria.bijoy@live.com>
2023-03-25 03:13:47 +0600

    Developing discount.proto ProtoBuf file for Exposing Crud Services Discount Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,6 +1,6 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
-using Discount.Grpc.Services;
+//using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +16,7 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.MigrateDatabase<Program>();
