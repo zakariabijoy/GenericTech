904e6d4f897a566465694e16d5629f60da38fdf9
zakariabijoy <zakaria.bijoy@live.com>
2023-03-25 03:33:36 +0600

    Developing DiscountService class to Implement Grpc Proto Service Methods

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,44 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly IDiscountRepository _discountRepository;
+    private readonly ILogger<DiscountService> _logger;
+
+    public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+    {
+        _discountRepository = discountRepository;
+        _logger = logger;
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _discountRepository.GetDiscount(request.ProductName);
+        if(coupon == null)
+        {
+            throw new RpcException(new Status(statusCode: StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+        }
+
+        var couponModel  =  _mapper.Map<CouponModel>(coupon);
+        return couponModel;
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
