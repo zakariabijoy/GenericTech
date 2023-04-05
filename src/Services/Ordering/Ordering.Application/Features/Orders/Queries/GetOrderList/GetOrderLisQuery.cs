e2cf693b311ca9b01e38e433b5d109e184b5f4c2
zakariabijoy <zakaria.bijoy@live.com>
2023-04-06 01:37:53 +0600

    Developing Ordering.Application Layer- Application Features - GetOrdersListQuery

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderLisQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList;
+
+public class GetOrderLisQuery : IRequest<List<OrderDto>>
+{
+    public string Username { get; set; }
+
+    public GetOrderLisQuery(string userName)
+    {
+        Username = userName ?? throw new ArgumentNullException(nameof(userName)) ;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderLisQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList;
+
+public class GetOrderLisQueryHandler : IRequestHandler<GetOrderLisQuery, List<OrderDto>>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+
+    public GetOrderLisQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+    {
+        _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task<List<OrderDto>> Handle(GetOrderLisQuery request, CancellationToken cancellationToken) => _mapper.Map<List<OrderDto>>(await _orderRepository.GetOrdersByUserName(request.Username));
+}
