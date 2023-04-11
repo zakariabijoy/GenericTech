08528388e533e7ae07ee8262ad646ad437ab0bc5
zakariabijoy <zakaria.bijoy@live.com>
2023-04-12 03:46:11 +0600

    Developing Ordering.Application Layer - Application Behaviours

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<IRequest>> validators)
+    {
+        _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if(_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var failure = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+            if(failure.Count != 0)
+                throw new ValidationException(failure);
+        }
+
+        return await next();
+    }
+}
