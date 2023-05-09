d67fb7ada885945dab6185abbcaee714690a2bb6
zakariabijoy <zakaria.bijoy@live.com>
2023-05-10 03:23:02 +0600

    Developing Ordering.Infrastructure Layer in Clean Architecture - Repositories And Email Service

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace Ordering.Infrastructure.Mail;
+
+public class EmailService : IEmailService
+{
+    private readonly ILogger<EmailService> _logger;
+    private readonly EmailSettings _emailSettings;
+
+    public EmailService(ILogger<EmailService> logger, IOptions<EmailSettings> emailSettings)
+    {
+        _logger = logger;
+        _emailSettings = emailSettings.Value;
+    }
+
+    public async Task<bool> SendEmail(Email email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+        var from = new EmailAddress(_emailSettings.FromAddress,_emailSettings.FromName);
+        var subject = email.Subject;
+        var to = new EmailAddress(email.To);
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, email.Body, email.Body);
+        var response = await client.SendEmailAsync(msg);
+        _logger.LogInformation("Email Sent");
+
+        if(response.StatusCode == HttpStatusCode.Accepted || response.StatusCode ==  HttpStatusCode.OK)
+            return true;
+
+        _logger.LogError("Email sending Failed");
+        return false;   
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+{
+    readonly OrderContext _orderContext;
+
+    public OrderRepository(OrderContext orderContext) :base(orderContext)
+    {
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName) => await _orderContext.Orders.Where(x => x.UserName == userName).ToListAsync();
+}
