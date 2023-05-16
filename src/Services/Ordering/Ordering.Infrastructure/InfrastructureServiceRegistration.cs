75086115bc4628967feb1d72fc4edbe9751e5f2f
zakariabijoy <zakaria.bijoy@live.com>
2023-05-17 02:16:21 +0600

    Developing Ordering.Infrastructure Layer - Infrastructure Service Registrations

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure;
+
+public static  class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuraton)
+    {
+        services.AddDbContext<OrderContext>(options => options.UseSqlServer(configuraton.GetConnectionString("OrderingConnectionString")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        services.Configure<EmailSettings>(c => configuraton.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+        return services;
+    }
+}
