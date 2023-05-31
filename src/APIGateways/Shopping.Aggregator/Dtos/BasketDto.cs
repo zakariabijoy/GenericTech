ab5e638dc0b64c64f1501693087c1ff1a9bf2acd
zakariabijoy <zakaria.bijoy@live.com>
2023-06-01 00:47:27 +0600

    Developing Dto Model Class for Api Aggreation Operations

---
+++ src/APIGateways/Shopping.Aggregator/Dtos/BasketDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Dtos;
+
+public class BasketDto
+{
+    public string UserName { get; set; }
+    public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+    public decimal TotalPrice { get; set; }
+}
---
+++ src/APIGateways/Shopping.Aggregator/Dtos/ShoppingDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Aggregator.Dtos;
+
+public class ShoppingDto
+{
+    public string UserName { get; set; }
+    public BasketDto BasketWithProducts { get; set; }
+    public IEnumerable<OrderResponseDto> Orders { get; set; }
+}
