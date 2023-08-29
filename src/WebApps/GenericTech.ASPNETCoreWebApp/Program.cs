070bfe220ea3e12e25b99a05e49c16a0ceae23c9
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-08-30 01:57:14 +0600

    Intall and Configure SeriLog For ElasticSearch and Kibana Sink Integration -ASPNetCoreWebApp

--- src/WebApps/GenericTech.ASPNETCoreWebApp/Program.cs
+++ src/WebApps/GenericTech.ASPNETCoreWebApp/Program.cs
@@ -1,8 +1,30 @@
 using GenericTech.ASPNETCoreWebApp.Services;
 using GenericTech.ASPNETCoreWebApp.Services.Interfaces;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+
+//serilog configuration
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(
+            new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+            {
+                IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".","-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".","-")}-logs-{DateTime.UtcNow:yyy-MM}",
+                AutoRegisterTemplate = true,
+                NumberOfShards = 2,
+                NumberOfReplicas = 1,
+            })
+        .Enrich.WithProperty("Environemtn", context.HostingEnvironment.EnvironmentName)
+        .ReadFrom.Configuration(context.Configuration);
+});
 // Add services to the container.
 
 //Configure Typed Clients with IHttpClientFactory
