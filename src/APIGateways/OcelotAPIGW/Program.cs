8d725cfa9b3fe595c030954316c2d206f008207d
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-08 02:03:53 +0600

    Adding SeriLog for OcelotApiGw Microservices for Logging on ElasticSearch

--- src/APIGateways/OcelotAPIGW/Program.cs
+++ src/APIGateways/OcelotAPIGW/Program.cs
@@ -1,8 +1,13 @@
+using Common.Logging;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//serilog configuration
+builder.Host.UseSerilog(SeriLogger.Configure);
 
 builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
