54a2889467027a699929748287afe6db26319c8c
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-09-08 01:17:13 +0600

    Adding SeriLog for ShoppingAggregator Microservices for Logging on ElasticSearch

--- src/APIGateways/Shopping.Aggregator/Program.cs
+++ src/APIGateways/Shopping.Aggregator/Program.cs
@@ -1,20 +1,30 @@
+using Common.Logging;
+using Serilog;
 using Shopping.Aggregator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//serilog configuration
+builder.Host.UseSerilog(SeriLogger.Configure);
 
 // Add services to the container.
 
 //Configure Typed Clients with IHttpClientFactory
 var configuration = builder.Configuration;
 
+builder.Services.AddTransient<LoggingDelegatingHandler>();
+
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
-                c.BaseAddress = new Uri(configuration["APISettings:CatalogUrl"]));
+    c.BaseAddress = new Uri(configuration["APISettings:CatalogUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-    c.BaseAddress = new Uri(configuration["APISettings:BasketUrl"]));
+    c.BaseAddress = new Uri(configuration["APISettings:BasketUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(configuration["APISettings:OrderingUrl"]));
+    c.BaseAddress = new Uri(configuration["APISettings:OrderingUrl"]))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
